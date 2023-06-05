49e7b7d86d4e26117e648e89dcd8b73f218d83d8
Parthiban024 <parthiban@objectways.com>
2023-06-05 16:02:53 +0530

    new

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -31,7 +31,7 @@ employeeName, employeeId, emailId, systemNo, systemType, systemTypetwo, unitNo, 
     }
   })
   const mailOptions = {
-    from: `${req.body.employeeName}`,
+    from: `${employeeName}`,
     to: 'parthiban@objectways.com',
     cc: 'parthibaneee7548@gmail.com',
     subject: `New Ticket from the employee ID ${req.body.employeeId}`,
